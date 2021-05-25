a79d89a34b7f8b11c39fd2b324b9bc4874e30551
Allen <allen200118@gmail.com>
2021-05-25 20:54:34 +0400

    Day 1 project completed

---
+++ day1.py
@@ -0,0 +1,6 @@
+
+#Band Name Generator
+
+city_name=input("What's the name of the City you grew up in? \n ")
+pet_name=input("What's the name of your first pet? \n ")
+print("Your Band Name could be :\n" + city_name + " " + pet_name)
