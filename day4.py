e1b16847b0cc1e7139fee16f43e12a8e459234f5
Allen <allen200118@gmail.com>
2021-06-06 22:48:54 +0400

    day 4 completed

---
+++ day4.py
@@ -0,0 +1,72 @@
+#Rock Paper Scissors Game
+import random
+import sys
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+print("Welcome to Rock, Paper, Scissors!")
+choice=int(input("Enter 0 for Rock, 1 for Paper and , 2 for Scissors :"))
+
+if choice>2:
+    sys.exit("Invalid choice!")
+
+
+elif choice == 0:
+    print(rock)
+
+elif choice == 1:
+    print(paper)
+
+elif choice == 2:
+    print(scissors)
+
+computer_choice=random.randint(0,2)
+
+
+print("Computer played :")
+
+if computer_choice == 0:
+    print(rock)
+
+elif computer_choice == 1:
+    print(paper)
+
+elif computer_choice == 2:
+    print(scissors)
+
+#Choice Comparing
+
+if computer_choice==choice:
+    print("Try Again!")
+elif computer_choice == 2 and choice==0:
+    print("You Win!")
+
+elif computer_choice>choice:
+    print("Computer Wins!")
+
+
+
